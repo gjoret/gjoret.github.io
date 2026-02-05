68a3150ba6b12222edf769cb204ed5235d64ed61
Gwenaël Joret <gjoret@gmail.com>
2026-02-05 01:31:03 +0100

    _

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9626de07.js',
+                path_to_root + 'searchindex-01f4afce.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
